9612a6cfa388e0666ff102db6c991a3cd8cad5ff
Aki Kurvinen <aki.kurvinen@hotmail.fi>
2022-12-01 09:10:39 +0200

    add tests

--- cypress.config.js
+++ cypress.config.js
@@ -1,17 +1,11 @@
-require('dotenv').config()
 const { defineConfig } = require("cypress");
 
-
-
 module.exports = defineConfig({
+  defaultCommandTimeout: 30000,
   e2e: {
     setupNodeEvents(on, config) {
-      config.env = {
-        ...process.env,
-        ...config.env
-      }
-      return config 
+      // implement node event listeners here
     },
-    baseUrl: 'http://localhost:3000'
-  }
-})
+     baseUrl: 'http://localhost:3000'
+  },
+});
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,33 +1,55 @@
 /* eslint-disable no-undef */
 /* eslint-disable jest/valid-expect */
 /// <reference types="cypress" />
-require('dotenv').config()
+
+const ROBOT_USERNAME='testrobot'
+const ROBOT_PASSWORD='Testrobot@ttack'
 
 describe('Login', () => {
 
    it('should login', () => {
       cy.visit('/')
+
+      // find login form
       cy.get('#username');
       cy.get('#password');
 
-      // cy.get('[data-cy=login_submit_btn]').click();
-
+      // find signup form
       cy.get('[data-cy=switch_to_signup_btn]').click();
       cy.get('#username');
       cy.get('#email');
       cy.get('#password');
       cy.get('#password');
 
-      // cy.get('[data-cy=signup_submit_btn]').click();
-
-      let username = Cypress.env('ROBOT_USERNAME')
-      let password = Cypress.env('ROBOT_PASSWORD')
-
+      // use login form
       cy.get('[data-cy=switch_to_login_btn]').click();
-      cy.get('#username').type(username);
-      cy.get('#password').type(password);
-
+      cy.get('#username').type(ROBOT_USERNAME);
+      cy.get('#password').type(ROBOT_PASSWORD);
       cy.get('[data-cy=login_submit_btn]').click();
 
+   })
+
+})
+describe('Test sidebar', () => {
+   it('should find sidebar items', () => {
+      // find and click sidebar buttons
+      const arr = ['account', 'cog', 'paragraph', 'text', 'layouts', 'colors']
+      arr.forEach(element => {
+         cy.get(`img[src*="${element}"]`).click()
+      });
+   
+   })
+})
+
+describe('Test layouts', () => {
+   it('should change page layout', () => {
+      // open and check layouts panel
+      cy.get(`img[src*="layouts"]`).click()
+      const arr = ['grid', 'photos', 'videos', 'slides']
+      arr.forEach(element => {
+         cy.get(`img[src*="${element}"]`).click()
+         cy.get('.ButtonUnstyled-root').contains('No').click()
+      });
+   
    })
 })
