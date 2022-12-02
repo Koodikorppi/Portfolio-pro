1efd015a327f2671b7623c9cf4674edb32767d69
Aki Kurvinen <aki.kurvinen@hotmail.fi>
2022-12-02 09:15:24 +0200

    update tests

--- README.md
+++ README.md
@@ -14,7 +14,6 @@ pip install -r requirements.txt
 ## Create .env file and fill out variables
 ```
 URL=api_url
-TOKEN=test_token
 USER_NAME=test_account_username
 PASS_WORD=test_account_password
 ```
--- tests/test_portfolio_pro.py
+++ tests/test_portfolio_pro.py
@@ -3,14 +3,11 @@ import requests
 load_dotenv() 
 import os
 
-AWS_ID = os.getenv("AWS_ID")
 URL = os.getenv("URL")
-AWS_TOKEN = os.getenv("AWS_TOKEN")
 USER_NAME = str(os.getenv("USER_NAME"))
 PASS_WORD = str(os.getenv("PASS_WORD"))
-assert AWS_ID, "AWS_ID is not set"
+
 assert URL, "URL is not set"
-assert AWS_TOKEN, "AWS_TOKEN is not set"
 assert USER_NAME, "USER_NAME is not set"
 assert PASS_WORD, "PASS_WORD is not set"
 
@@ -28,6 +25,5 @@ def test_post_login():
     assert len(json_res["userId"]) > 10
 
 def test_get_api():
-    headers = {'Authorization': f'AWS {AWS_ID}:{AWS_TOKEN}'}
-    r = requests.get(f'{URL}/', headers=headers)
+    r = requests.get(f'{URL}/')
     assert r.status_code == 401
